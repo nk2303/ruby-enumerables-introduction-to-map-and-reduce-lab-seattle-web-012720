7cd207eb81a6b05da4673496cce73df52b469186
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-13 08:02:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,7 +26,7 @@ end
 
   
 def reduce_to_total(source_array, starting_point)
-  starting_point === nil ? total = 0 : total = starting_point
+  starting_point == nil ? total = 0 : total = starting_point
   source_array.length.times do |item|
     total += source_array[item]
   end
