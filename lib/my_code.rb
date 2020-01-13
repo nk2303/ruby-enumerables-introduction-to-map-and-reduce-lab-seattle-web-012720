e58899ed2661bfdbf223c2c48e817c7c4b8288a0
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-13 07:09:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,3 +3,7 @@ def map_to_negativize(source_array)
 def map_to_no_change(source_array)
 def map_to_double(source_array)
 def map_to_square(source_array)
+  
+def reduce_to_total(source_array, starting_point)
+def reduce_to_all_true(source_array)
+def reduce_to_any_true(source_array)
