7bc79cef4969955bbfd6f2c44af1dc0f13001b9c
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-13 07:15:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,12 +26,13 @@ end
 
   
 def reduce_to_total(source_array, starting_point)
-end
-
-def reduce_to_all_true(source_array)
   
 end
 
+def reduce_to_all_true(source_array)
+  return source_array.all?
+end
+
 def reduce_to_any_true(source_array)
-  source_array.any?
+  return source_array.any?
 end
