7d7a07dc0a78552cc1b7ac9dc1c3ac90addc2f1d
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-13 07:12:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,12 +7,21 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
+  return source_array
 end
 
 def map_to_double(source_array)
+  source_array.length.times do |item|
+    source_array[item] = source_array[item]*2
+  end
+  return source_array
 end
 
 def map_to_square(source_array)
+  source_array.length.times do |item|
+    source_array[item] = source_array[item]**2
+  end
+  return source_array
 end
 
   
